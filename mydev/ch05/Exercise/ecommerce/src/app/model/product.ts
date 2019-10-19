2e59edceb7efcd23d225917ca8fe7199daf330d1
David Lehman <lemo@provide.net>
2019-10-19 12:13:41 -0400

    Finished Ch05 - Unit Testing

---
+++ mydev/ch05/Exercise/ecommerce/src/app/model/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    constructor(
+        public id: number,
+        public name: string,
+        public imagePath: string,
+        public price: number,
+        public onSale: boolean,
+        public quantityInCart: number
+    ){}
+}
