8e7884bcfa4ffeed778cd4b82f705d21f07e46bf
David Lehman <lemo@provide.net>
2019-10-24 06:00:12 -0400

    Ch07 FormBuilder

--- mydev/ch07/stock-market/src/app/stock/create-stock/create-stock.component.ts
+++ mydev/ch07/stock-market/src/app/stock/create-stock/create-stock.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Stock } from '../../model/stock';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-stock',
@@ -9,16 +8,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreateStockComponent {
 
-  public stockForm: FormGroup = new FormGroup({
-      name: new FormControl(null, Validators.required),
-      code: new FormControl(null, [Validators.required, Validators.minLength(2)]),
-      price: new FormControl(0, [Validators.required,Validators.min(0)])
-  });
-  constructor() { }
+  public stockForm: FormGroup;
+  constructor(private fb: FormBuilder) { 
+    this.createForm();
+  }
 
 onSubmit() {
   console.log('Stock Form Value', this.stockForm.value);
   }
 
+createForm() {
+  this.stockForm = this.fb.group({
+      name: [null, Validators.required],
+      code: [null, [Validators.required, Validators.minLength(2)]],
+      price: [0, [Validators.required, Validators.min(0)]]
+  });
+}
 
 }
